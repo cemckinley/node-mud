a4929a6401037426133b733d8f8e9d4b008440c5
Austin Happel <austin@austinhappel.com>
2013-08-10 15:19:04 -0700

    converted Gruntfile to tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 /*global module:false*/
 
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
 
 var DEV_PATH = 'client/dev';
@@ -13,218 +13,218 @@ var SERVER_APP_PATH = 'server';
 var path = require('path');
 var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
 var folderMount = function folderMount(connect, point) {
-  return connect.static(path.resolve(point));
+	return connect.static(path.resolve(point));
 };
 
 // Project configuration.
 grunt.initConfig({
-    qunit: {
-        files: [TEST_PATH + '/**/*.html']
-    },
-    regarde: {
-        css: {
-            files: [
-                DEV_PATH + '/_ui/css/**/*.scss',
-                DEV_PATH + '/_ui/css/**/*.css'
-            ],
-            tasks: ['compass:dev', 'copy:devcss', 'livereload']
-        },
-        js: {
-            files: [
-                DEV_PATH + '/_ui/js/**/*.js'
-            ],
-            tasks: ['copy:devjs', 'livereload']
-        },
-        hbs: {
-            files: [
-                DEV_PATH + '/_ui/hbs/**/*.hbs'
-            ],
-            tasks: ['handlebars:dev', 'livereload']
-        },
-        serverjs: {
-            files: [
-                'server/**/*.js'
-            ],
-            tasks: ['runscripts']
-        },
-        other: {
-            files: [
-                DEV_PATH + '/**/*.html',
-                DEV_PATH + '/_ui/img/**/*'
-            ],
-            tasks: ['copy:devall', 'livereload']
-        }
-    },
-    connect: {
-        dev: {
-            options: {
-                port: 8001,
-                hostname: null, // setting hostname to null allows requests at port 8001 locally for any host name (i.e. localhost, network IP or machine name)
-                base: TEMP_PATH,
-                middleware: function(connect, options) {
-                    return [lrSnippet, folderMount(connect, TEMP_PATH)];
-                }
-            }
-        }
-    },
-    jshint: {
-        options: {
-            curly: true,
-            eqeqeq: true,
-            immed: true,
-            latedef: true,
-            newcap: true,
-            noarg: true,
-            sub: true,
-            undef: true,
-            boss: true,
-            eqnull: true,
-            browser: true
-        },
-        files: grunt.file.expand([
-            DEV_PATH + '/_ui/js/**/*.js',
-            TEST_PATH + '/**/*.js',
-            '!' + TEST_PATH + '/qunit.js',
-            '!' + DEV_PATH + '/_ui/js/lib/**/*' // leave out 3rd party js in lib folder, since can't guarantee lint quality
-        ]),
-        globals: {}
-    },
-    compass: {
-        dev: {
-            options: {
-                cssDir: TEMP_PATH + '/_ui/css',
-                sassDir: DEV_PATH + '/_ui/css',
-                environment: 'development'
-            }
-        },
-        dist: {
-            options: {
-                cssDir: DIST_PATH + '/_ui/css',
-                sassDir: DEV_PATH + '/_ui/css',
-                environment: 'production'
-            }
-        }
-    },
-    handlebars: {
-        dev: {
-            options: {
-                namespace: 'NODEMUD.templates',
-                processName: function(filename) {
-                    var name = filename.split('/hbs/')[1].split('.hbs')[0]; // remove from hbs dir up, and remove .hbs extension
-                    return name;
-                }
-            },
-            files: [
-                {
-                    dest: TEMP_PATH + '/_ui/js/templates.js',
-                    src: DEV_PATH + '/_ui/hbs/**/*.hbs'
-                }
-            ]
-        },
-        dist: {
-            options: {
-                namespace: 'NODEMUD.templates'
-            },
-            files: [
-                {
-                    dest: DIST_PATH + '/_ui/js/templates.js',
-                    src: DEV_PATH + '/_ui/hbs/**/*.hbs'
-                }
-            ]
-        }
-    },
-    runscripts: {
-        websocket: [SERVER_APP_PATH + '/app.js'] // start server app node process
-    },
-    shell: {
-        startMongo: {
-            command: 'mongod', // start mongodb
-            options: {
-                async: true
-            }
-        }
-    },
+	qunit: {
+		files: [TEST_PATH + '/**/*.html']
+	},
+	regarde: {
+		css: {
+			files: [
+				DEV_PATH + '/_ui/css/**/*.scss',
+				DEV_PATH + '/_ui/css/**/*.css'
+			],
+			tasks: ['compass:dev', 'copy:devcss', 'livereload']
+		},
+		js: {
+			files: [
+				DEV_PATH + '/_ui/js/**/*.js'
+			],
+			tasks: ['copy:devjs', 'livereload']
+		},
+		hbs: {
+			files: [
+				DEV_PATH + '/_ui/hbs/**/*.hbs'
+			],
+			tasks: ['handlebars:dev', 'livereload']
+		},
+		serverjs: {
+			files: [
+				'server/**/*.js'
+			],
+			tasks: ['runscripts']
+		},
+		other: {
+			files: [
+				DEV_PATH + '/**/*.html',
+				DEV_PATH + '/_ui/img/**/*'
+			],
+			tasks: ['copy:devall', 'livereload']
+		}
+	},
+	connect: {
+		dev: {
+			options: {
+				port: 8001,
+				hostname: null, // setting hostname to null allows requests at port 8001 locally for any host name (i.e. localhost, network IP or machine name)
+				base: TEMP_PATH,
+				middleware: function(connect, options) {
+					return [lrSnippet, folderMount(connect, TEMP_PATH)];
+				}
+			}
+		}
+	},
+	jshint: {
+		options: {
+			curly: true,
+			eqeqeq: true,
+			immed: true,
+			latedef: true,
+			newcap: true,
+			noarg: true,
+			sub: true,
+			undef: true,
+			boss: true,
+			eqnull: true,
+			browser: true
+		},
+		files: grunt.file.expand([
+			DEV_PATH + '/_ui/js/**/*.js',
+			TEST_PATH + '/**/*.js',
+			'!' + TEST_PATH + '/qunit.js',
+			'!' + DEV_PATH + '/_ui/js/lib/**/*' // leave out 3rd party js in lib folder, since can't guarantee lint quality
+		]),
+		globals: {}
+	},
+	compass: {
+		dev: {
+			options: {
+				cssDir: TEMP_PATH + '/_ui/css',
+				sassDir: DEV_PATH + '/_ui/css',
+				environment: 'development'
+			}
+		},
+		dist: {
+			options: {
+				cssDir: DIST_PATH + '/_ui/css',
+				sassDir: DEV_PATH + '/_ui/css',
+				environment: 'production'
+			}
+		}
+	},
+	handlebars: {
+		dev: {
+			options: {
+				namespace: 'NODEMUD.templates',
+				processName: function(filename) {
+					var name = filename.split('/hbs/')[1].split('.hbs')[0]; // remove from hbs dir up, and remove .hbs extension
+					return name;
+				}
+			},
+			files: [
+				{
+					dest: TEMP_PATH + '/_ui/js/templates.js',
+					src: DEV_PATH + '/_ui/hbs/**/*.hbs'
+				}
+			]
+		},
+		dist: {
+			options: {
+				namespace: 'NODEMUD.templates'
+			},
+			files: [
+				{
+					dest: DIST_PATH + '/_ui/js/templates.js',
+					src: DEV_PATH + '/_ui/hbs/**/*.hbs'
+				}
+			]
+		}
+	},
+	runscripts: {
+		websocket: [SERVER_APP_PATH + '/app.js'] // start server app node process
+	},
+	shell: {
+		startMongo: {
+			command: 'mongod', // start mongodb
+			options: {
+				async: true
+			}
+		}
+	},
 
 
-    // build config
-    clean: {
-        dev: [TEMP_PATH],
-        dist: [DIST_PATH]
-    },
-    copy: {
-        devcss: {
-            files: [
-                {
-                    expand: true,
-                    src: [
-                        '_ui/css/**/*.css'
-                    ],
-                    dest: TEMP_PATH + '/',
-                    cwd: DEV_PATH + '/'
-                }
-            ]
-        },
-        devjs: {
-            files: [
-                {
-                    expand: true,
-                    src: [
-                        '_ui/js/**/*.js'
-                    ],
-                    dest: TEMP_PATH + '/',
-                    cwd: DEV_PATH + '/'
-                }
-            ]
-        },
-        devall: {
-            files: [
-                {
-                    expand: true,
-                    src: [
-                        '**',
-                        '!**/*.scss', // copy over everything except scss, which will be processed/copied with compass
-                        '!**/hbs/**' // don't copy over handlebar directory, since they are precompiled into _ui/js/templates.js
-                    ],
-                    dest: TEMP_PATH + '/',
-                    cwd: DEV_PATH + '/'
-                }
-            ]
-        },
-        dist: {
-            files: [
-                {
-                    expand: true,
-                    src: [
-                        'client/**',
-                        '!_ui/css/**', // copy over everything except css, js, & handlebars dir which will be processed/copied with other tasks
-                        '!_ui/js/**',
-                        '!_ui/hbs/**'
-                    ],
-                    dest: DIST_PATH + '/',
-                    cwd: DEV_PATH + '/'
+	// build config
+	clean: {
+		dev: [TEMP_PATH],
+		dist: [DIST_PATH]
+	},
+	copy: {
+		devcss: {
+			files: [
+				{
+					expand: true,
+					src: [
+						'_ui/css/**/*.css'
+					],
+					dest: TEMP_PATH + '/',
+					cwd: DEV_PATH + '/'
+				}
+			]
+		},
+		devjs: {
+			files: [
+				{
+					expand: true,
+					src: [
+						'_ui/js/**/*.js'
+					],
+					dest: TEMP_PATH + '/',
+					cwd: DEV_PATH + '/'
+				}
+			]
+		},
+		devall: {
+			files: [
+				{
+					expand: true,
+					src: [
+						'**',
+						'!**/*.scss', // copy over everything except scss, which will be processed/copied with compass
+						'!**/hbs/**' // don't copy over handlebar directory, since they are precompiled into _ui/js/templates.js
+					],
+					dest: TEMP_PATH + '/',
+					cwd: DEV_PATH + '/'
+				}
+			]
+		},
+		dist: {
+			files: [
+				{
+					expand: true,
+					src: [
+						'client/**',
+						'!_ui/css/**', // copy over everything except css, js, & handlebars dir which will be processed/copied with other tasks
+						'!_ui/js/**',
+						'!_ui/hbs/**'
+					],
+					dest: DIST_PATH + '/',
+					cwd: DEV_PATH + '/'
 
-                }
-            ]
-        }
-    },
-    concat: {
-        dist: {
-            src: [DEV_PATH + '/_ui/js/**/*.js'],
-            dest: DIST_PATH + '/_ui/js/scripts.js'
-        }
-    },
-    uglify: {
-        dist: {
-            dest: DIST_PATH + '/_ui/js/scripts.min.js',
-            src: [DIST_PATH + '/_ui/js/scripts.js']
-        }
-    },
-    // replace js and css link build blocks within specified files with their concatenated versions
-    replacelinks: {
-        files: [
-            DIST_PATH + '/*.html'
-        ]
-    }
+				}
+			]
+		}
+	},
+	concat: {
+		dist: {
+			src: [DEV_PATH + '/_ui/js/**/*.js'],
+			dest: DIST_PATH + '/_ui/js/scripts.js'
+		}
+	},
+	uglify: {
+		dist: {
+			dest: DIST_PATH + '/_ui/js/scripts.min.js',
+			src: [DIST_PATH + '/_ui/js/scripts.js']
+		}
+	},
+	// replace js and css link build blocks within specified files with their concatenated versions
+	replacelinks: {
+		files: [
+			DIST_PATH + '/*.html'
+		]
+	}
 });
 
 // grunt 3rd party plugins
